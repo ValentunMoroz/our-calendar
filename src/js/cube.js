5f2586046f63f3da5e77269ad33a24c8291d2849
Valentun2 <pocomaxawrkt@gmail.com>
2023-09-10 19:28:28 +0300

    Comment text here...

--- src/js/cube.js
+++ src/js/cube.js
@@ -11,10 +11,10 @@ const cubeItem = document.querySelector(".cube__item")
 const guard = document.querySelector('.guard')
 let rotateX=0
 let rotateY=0
-// setInterval(()=>{
-//     rotateY +=8
-//     document.querySelector('.cube__container').style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
-// },100);
+setInterval(()=>{
+    rotateY +=8
+    document.querySelector('.cube__container').style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+},100);
  function hanglerKey (e){
     e.preventDefault()
 if(e.keyCode === 37){
@@ -68,38 +68,38 @@ let x = 360
 let z = 180
 
 
-// const s = setInterval(() => {
-//     if(x===340){
+const s = setInterval(() => {
+    if(x===40){
     
-//         clearInterval(s)
-//         cube.classList.replace("cube","cube-n")
-//         cubeContainer.classList.replace("cube__container","cube__container-n")
-//            cubeAll[0].classList.add("cube__item","cube__item-n")
-//          cubeAll[1].classList.add("cube__item","cube__item-n")
-//          cubeAll[2].classList.add("cube__item","cube__item-n")
-//          cubeAll[3].classList.add("cube__item","cube__item-n")
-//           cubeAll[4].classList.add("cube__item","cube__item-n")
-//       cubeAll[5].classList.add("cube__item","cube__item-n")
-//       cubeAll[0].style.transform =` none`
-//     cubeAll[1].style.transform =` none`
-//     cubeAll[2].style.transform =` none`
-//     cubeAll[3].style.transform =` none`
-//     cubeAll[4].style.transform =` none`
-//     cubeAll[5].style.transform =` none`
-//     cube.style.width= "100%"
-//       return
+        clearInterval(s)
+        cube.classList.replace("cube","cube-n")
+        cubeContainer.classList.replace("cube__container","cube__container-n")
+           cubeAll[0].classList.add("cube__item","cube__item-n")
+         cubeAll[1].classList.add("cube__item","cube__item-n")
+         cubeAll[2].classList.add("cube__item","cube__item-n")
+         cubeAll[3].classList.add("cube__item","cube__item-n")
+          cubeAll[4].classList.add("cube__item","cube__item-n")
+      cubeAll[5].classList.add("cube__item","cube__item-n")
+      cubeAll[0].style.transform =` none`
+    cubeAll[1].style.transform =` none`
+    cubeAll[2].style.transform =` none`
+    cubeAll[3].style.transform =` none`
+    cubeAll[4].style.transform =` none`
+    cubeAll[5].style.transform =` none`
+    cube.style.width= "100%"
+      return
         
-//     }
-//     cube.style.width = `${x}px`
-//     cube.style.height = `${x}px`
-//     cubeAll[0].style.transform =` rotateX(90deg) translateZ(${z}px)`
-//     cubeAll[1].style.transform =` rotateY(90deg) translateZ(${z}px)`
-//     cubeAll[2].style.transform =` rotateY(-90deg) translateZ(${z}px)`
-//     cubeAll[3].style.transform =` rotateX(-90deg) translateZ(${z}px)`
-//     cubeAll[4].style.transform =` translateZ(${z}px)`
-//     cubeAll[5].style.transform =` rotateY(180deg) translateZ(${z}px)`
-//     x-=10
-//     z -=5
+    }
+    cube.style.width = `${x}px`
+    cube.style.height = `${x}px`
+    cubeAll[0].style.transform =` rotateX(90deg) translateZ(${z}px)`
+    cubeAll[1].style.transform =` rotateY(90deg) translateZ(${z}px)`
+    cubeAll[2].style.transform =` rotateY(-90deg) translateZ(${z}px)`
+    cubeAll[3].style.transform =` rotateX(-90deg) translateZ(${z}px)`
+    cubeAll[4].style.transform =` translateZ(${z}px)`
+    cubeAll[5].style.transform =` rotateY(180deg) translateZ(${z}px)`
+    x-=10
+    z -=5
 // if(x==50){
 //     cubeAll[0].textContent=""
 //     cubeAll[1].textContent=""
@@ -109,4 +109,4 @@ let z = 180
 //     cubeAll[5].textContent=""
 // }
 
-// }, 1000);
+}, 1000);
